67186061aea9e0412bf6d247d710b2573c6ae532
drprasad <edu.dr.prasad@live.com>
2016-10-19 23:02:03 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+showDivs(0);
+function showDivs(n)
+{
+	var n;
+	var x = document.getElementsByClassName("myBox");
+	var quotes = document.getElementById("quotes")
+	var i;
+
+	for(i=0; i < x.length; i++)
+	{
+		x[i].style.display = "none";
+	}
+	if(n > 0)
+	{
+		x[n - 1].style.display = "block";
+		quotes.style.display = "none";
+	}
+}
+	
